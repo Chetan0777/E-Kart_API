08ec611da64def57fe5aa20aa38910c58855016c
Chetan0777 <coderchetan1@gmail.com>
2022-11-29 23:01:59 +0530

    finally start pushing

---
+++ E-kart/src/main/java/com/ekart/model/Address.java
@@ -0,0 +1,14 @@
+package com.ekart.model;
+
+import lombok.Data;
+
+@Data
+public class Address {
+	
+	private Integer id;
+	private String city;
+	private String state;
+	private String country;
+	private String pincode;
+
+}
