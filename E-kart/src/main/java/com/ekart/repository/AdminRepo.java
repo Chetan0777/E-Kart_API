e9b58c7cdd3ffeeaa1fb918fa9e7da087ea43d43
Chetan0777 <coderchetan1@gmail.com>
2022-12-01 16:26:15 +0530

    done

---
+++ E-kart/src/main/java/com/ekart/repository/AdminRepo.java
@@ -0,0 +1,16 @@
+package com.ekart.repository;
+
+import java.util.List;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ekart.model.Admin;
+
+@Repository
+public interface AdminRepo extends JpaRepository<Admin, Integer>{
+	
+	public List<Admin> findByAdminMobile(String adminMobile);
+
+}
